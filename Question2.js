5d59215a2cb140209a238386e06809200cd6ec62
Ainst77 <61785626+Ainst77@users.noreply.github.com>
2020-10-18 10:18:41 +0200

    Update Question2.js

--- Question2.js
+++ Question2.js
@@ -1,14 +1,5 @@
 //Question 2
 
-//Add the following to a page called question2.html:
-
-//Make a call to https://api.rawg.io/api/games?dates=1999-01-01,1999-12-31&ordering=-rating.
-
-//For each game object in the results, create HTML that displays at least 3 properties from the objects. You can include an image property or leave it out.
-
-//Link from each game to question3.html and pass the id of each game in the querystring.
-
-
 
 const url = "https://api.rawg.io/api/games?dates=1999-01-01,1999-12-31&ordering=-rating";
 const gameinfoContainer = document.querySelector(".container");
