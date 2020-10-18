73538806caba1e45131c836707f375e54684ad3a
Ainst77 <61785626+Ainst77@users.noreply.github.com>
2020-10-18 10:05:32 +0200

    Add files via upload

---
+++ question1.js
@@ -0,0 +1,19 @@
+//Question 1
+
+//Convert the function below to an arrow function:
+
+//function division(a, b) {
+//    return a % b;
+//}
+ 
+const division = (a, b) => a / b;
+const svar = division (15, 3);
+console.log(svar);
+
+//  const division = (a, b) => {
+//     return a / b;
+//  } 
+//  const svar = division (15, 3);
+//  console.log(svar);
+
+   
